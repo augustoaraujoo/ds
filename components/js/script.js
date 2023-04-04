aec7eea158620e0c950badc44b2135948f34f6c5
augustoroocha <augustinho_darocha1311@hotmail.com>
2023-04-03 21:14:55 -0300

    kkkk

--- components/js/script.js
+++ components/js/script.js
@@ -1,4 +1,6 @@
 window.onload = function () {
+    alert('site em construção tmj é nois')
+    alert('roubamo o códigoEfodacekkkkkk')
     document.querySelector(".menuMobile").addEventListener("click", function () {
         if (document.querySelector(".menu nav ul").style.display == 'flex') {
             document.querySelector(".menu nav ul").style.display = 'none';
